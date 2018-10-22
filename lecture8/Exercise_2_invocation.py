d55d8fd8845ede95e84d1d46b7885a945447e921
Ivhen <jstudyprojects@gmail.com>
2018-10-22 15:34:43 +0300

    Chapter8 Exercise2 done

---
+++ lecture8/Exercise_2_invocation.py
@@ -0,0 +1,10 @@
+"""
+Exercise 2  http://greenteapress.com/thinkpython2/html/thinkpython2009.html
+
+There is a string method called count that is similar to the function in Section 8.7. Read the documentation of this method and write an invocation that counts the number of a’s in 'banana'.
+
+"""
+
+def count_(word, sub, start = 0, end = None):
+    return word.count(sub, start, end)
+
